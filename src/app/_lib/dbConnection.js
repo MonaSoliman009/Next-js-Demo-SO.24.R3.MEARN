0a4cbc963b6173bb49db1ff31c1d96137a7273d7
MonaSoliman009 <monasoliman009@gmail.com>
2024-09-17 11:55:29 +0300

    change db connection

--- src/app/_lib/dbConnection.js
+++ src/app/_lib/dbConnection.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 export function dbConnection(){
     console.log(process.env.SECRET);
     
-    // mongoose.connect(`mongodb+srv://monsoliman:${process.env.SECRET}@cluster0.ag3im.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`).then(()=>{
+    mongoose.connect(`mongodb+srv://monsoliman:${process.env.SECRET}@cluster0.ag3im.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`).then(()=>{
 
-    mongoose.connect('mongodb://127.0.0.1:27017/MEARNSONext').then(()=>{
+    // mongoose.connect('mongodb://127.0.0.1:27017/MEARNSONext').then(()=>{
         console.log("connected to db");
         
     }).catch((err)=>{
