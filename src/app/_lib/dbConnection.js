76b49d0919a0d15650ef679824699da2bdbc6206
MonaSoliman009 <monasoliman009@gmail.com>
2024-09-17 08:39:45 +0300

    Connected to atlas

--- src/app/_lib/dbConnection.js
+++ src/app/_lib/dbConnection.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
 
 export function dbConnection(){
-  
-    mongoose.connect('mongodb://127.0.0.1:27017/MEARNSONext').then(()=>{
+    
+    mongoose.connect(`mongodb+srv://monsoliman:${process.env.SECRET}@cluster0.ag3im.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`).then(()=>{
+
+    // mongoose.connect('mongodb://127.0.0.1:27017/MEARNSONext').then(()=>{
         console.log("connected to db");
         
     }).catch((err)=>{
